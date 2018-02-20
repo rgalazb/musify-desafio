c40514a253ff29eab5ada84eeca13e2e03dd33d7
rgalazb <galaz.becerra@gmail.com>
2018-02-20 15:27:00 -0300

    root añadido

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
     resources :songs, only: [:create, :destroy]
   end
   resources :users
+
+
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
